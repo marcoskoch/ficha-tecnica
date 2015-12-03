7ff0a578d1d462901e703ca5b3ffae58fb278c6a
Felipe Ribeiro De Moraes <felipemoraes920@gmail.com>
2015-12-03 16:37:14 -0200

    Adicionado Controller, Model, html, da tela inicial

---
+++ FichaTecnica/FichaTecnica/Models/TelaInicialModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FichaTecnica.Dominio;
+
+namespace FichaTecnica.Models
+{
+    public class TelaInicialModel
+    {
+        public List<Projeto> projetos { get; set; }
+        public List<int> QuantidadeMembros { get; set; }
+
+        public TelaInicialModel()
+        {
+
+        }
+
+    }
+    
+}
