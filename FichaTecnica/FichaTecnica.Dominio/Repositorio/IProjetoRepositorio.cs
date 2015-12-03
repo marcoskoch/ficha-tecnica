0a7b3571cb8eeeafb760d20a8cddcf5594349242
Felipe Ribeiro De Moraes <felipemoraes920@gmail.com>
2015-12-03 16:36:37 -0200

    Adicinado estrutura base do Projeto repositorio

---
+++ FichaTecnica/FichaTecnica.Dominio/Repositorio/IProjetoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FichaTecnica.Dominio.Repositorio
+{
+   public interface IProjetoRepositorio
+    {
+        List<Projeto> BuscarProjetosDoUsuario(int IDUsuario);
+    }
+}
--- FichaTecnica/FichaTecnica.Repositorio.EF/ProjetoRepositorio.cs
+++ FichaTecnica/FichaTecnica.Repositorio.EF/ProjetoRepositorio.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FichaTecnica.Dominio.Repositorio;
+using FichaTecnica.Dominio;
 
 namespace FichaTecnica.Repositorio.EF
 {
-    class ProjetoRepositorio
+   public class ProjetoRepositorio : IProjetoRepositorio
     {
+        private readonly BaseDeDados dataBase = new BaseDeDados();
+
+        public List<Projeto> BuscarProjetosDoUsuario(int IDUsuario)
+        {
+            return null;
+        }
     }
 }
