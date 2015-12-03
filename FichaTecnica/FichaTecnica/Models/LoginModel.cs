81ab254e9729fc87c47fa95518436bd7d90eb2e0
Marcos Koch <marcos_koch@hotmail.com>
2015-12-03 13:46:44 -0200

    feat(FichaTecnica) - Adicionado bootstrap e pagina master.html

---
+++ FichaTecnica/FichaTecnica/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FichaTecnica.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Senha { get; set; }
+    }
+}
