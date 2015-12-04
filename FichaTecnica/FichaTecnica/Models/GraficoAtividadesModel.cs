84cde0b112aabb42ecb76f243ade092140e65436
Renan Vaz <renanp.vaz@outlook.com>
2015-12-04 15:25:43 -0200

    adicionar listagem teste dos commits

---
+++ FichaTecnica/FichaTecnica/Models/GraficoAtividadesModel.cs
@@ -0,0 +1,19 @@
+﻿using FichaTecnica.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FichaTecnica.Models
+{
+    public class GraficoAtividadesModel
+    {
+
+        public List<LinkFork> Links { get; private set; }
+
+        public GraficoAtividadesModel(List<LinkFork> links)
+        {
+            Links = links;
+        }
+    }
+}
