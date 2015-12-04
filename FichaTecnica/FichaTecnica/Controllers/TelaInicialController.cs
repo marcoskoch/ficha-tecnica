3f086644c87ca02d21f5677b7165e7b1fde11e92
Felipe Ribeiro De Moraes <felipemoraes920@gmail.com>
2015-12-04 01:32:22 -0300

    Adicionado TelaInicialController

--- FichaTecnica/FichaTecnica/Controllers/TelaInicialController.cs
+++ FichaTecnica/FichaTecnica/Controllers/TelaInicialController.cs
@@ -1,4 +1,5 @@
 ﻿using FichaTecnica.Dominio;
+using FichaTecnica.Dominio.Repositorio;
 using FichaTecnica.Models;
 using FichaTecnica.Seguranca.Filters;
 using System;
@@ -11,10 +12,24 @@ namespace FichaTecnica.Controllers
 {
     public class TelaInicialController : Controller
     {
+        private IProjetoRepositorio dataBase = new FichaTecnica.Repositorio.EF.ProjetoRepositorio();
+
         [Autorizador]
-        public ActionResult TelaInicial()
+        public ActionResult TelaInicial(/*int idUsuario*/)
         {
-            return View();
+            int IDUsuario = 1;
+            List<Projeto> projetos = dataBase.BuscarProjetosDoUsuario(IDUsuario);
+
+            TelaInicialModel model = new TelaInicialModel();
+            model.projetos = new List<Projeto>();
+            model.projetos = projetos;
+
+            return View(model);
+        }
+
+        public ActionResult ProjetoDetalhe(int idProjeto)
+        {
+            return null;
         }
     }
 }
