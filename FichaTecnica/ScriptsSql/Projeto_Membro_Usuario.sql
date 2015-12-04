14e1e193b8242b90dae29a661ec512447ea524b3
Felipe Ribeiro De Moraes <felipemoraes920@gmail.com>
2015-12-04 17:35:44 -0200

    Adicionado novos scripts para o banco

--- FichaTecnica/ScriptsSql/Projeto_Membro_Usuario.sql
+++
@@ -1,3 +0,0 @@
-insert into projeto values ('Projeto Renner', 12/12/2015, 'Projeto da renner', 1);
-insert into membro values ('HeMan', 'HeMane@mail.com', 05-06-1992, 05-06-2000,'44344','',1);
-insert into cargo values ('Matador');
--- FichaTecnica/ScriptsSql/usuario.sql
+++
@@ -1,4 +0,0 @@
-insert into Permissao (Descricao) values ('Gerente');
--- senha do usuario '123';
-insert into Usuario (Nome, Email, Senha, IdPermissao) 
-	values ('Goku', 'gerente@cwi.com.br', '8502CD3ED213666DC2B730501A2A48B3', 1);
