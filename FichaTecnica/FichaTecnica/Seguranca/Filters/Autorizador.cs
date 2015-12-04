b7a3c5163212462aaa13fbfc48b490388aa6d079
Marcos Koch <marcos_koch@hotmail.com>
2015-12-04 14:52:23 -0200

    feat(FichaTecnica) adicionado retorno com mensagem de erro ao tentar acessar sistema sem estar logado. Adicionado teste de criptografia da senha

--- FichaTecnica/FichaTecnica/Seguranca/Filters/Autorizador.cs
+++ FichaTecnica/FichaTecnica/Seguranca/Filters/Autorizador.cs
@@ -6,6 +6,7 @@ using System.Security.Principal;
 using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace FichaTecnica.Seguranca.Filters
 {
@@ -15,16 +16,35 @@ namespace FichaTecnica.Seguranca.Filters
         {
             UsuarioLogado usuarioLogado = filterContext.HttpContext.Session["USUARIO_LOGADO"] as UsuarioLogado;
 
-            var identidade = new GenericIdentity(usuarioLogado.Email);
-            List<string> roles = new List<string>();
-            roles.Add(usuarioLogado.Perfil);
+            if (usuarioLogado != null)
+            {
+                var identidade = new GenericIdentity(usuarioLogado.Email);
+                List<string> roles = new List<string>();
+                roles.Add(usuarioLogado.Perfil);
 
-            var principal = new GenericPrincipal(identidade, roles.ToArray());
+                var principal = new GenericPrincipal(identidade, roles.ToArray());
 
-            Thread.CurrentPrincipal = principal;
-            HttpContext.Current.User = principal;
+                Thread.CurrentPrincipal = principal;
+                HttpContext.Current.User = principal;
 
-            base.OnAuthorization(filterContext);
+                base.OnAuthorization(filterContext);
+            }
+            else
+            {
+                filterContext.Controller.TempData.Add("MensagemErro", "Autentique-se primeiro");
+                RedirecionarParaLogin(filterContext);
+            }
+
+        }
+
+        private void RedirecionarParaLogin(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                        {"action", "Index" },
+                        {"controller", "Login" }
+                });
         }
     }
 }
