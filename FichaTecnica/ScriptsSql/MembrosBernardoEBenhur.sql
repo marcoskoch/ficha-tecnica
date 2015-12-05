a2b7c10cb7cea5b31ca3189bf4372b57cd38cf2d
Renan Vaz <renanp.vaz@outlook.com>
2015-12-05 01:55:25 -0200

    fix listagem dos commits mais recentes

---
+++ FichaTecnica/ScriptsSql/MembrosBernardoEBenhur.sql
@@ -0,0 +1,7 @@
+insert into Membro(Nome, Email, DataDeNascimento, DataCriacao, Telefone, IdCargo) values('Ben-hur', 'benhurott@gmail.com', '1988-05-12', getdate(), '9348538', 2)
+insert into LinkFork(IdProjeto, IdMembro, URL) values (2, 3, '/benhurott/gh-x9/')
+
+insert into Membro(Nome, Email, DataDeNascimento, DataCriacao, Telefone, IdCargo) values('Bernardo Bosak de Rezende', 'bernardobrezende@gmail.com', '1981-05-12', getdate(), '9331558', 1)
+insert into LinkFork(IdProjeto, IdMembro, URL) values (1, 4, '/bernardobrezende/turborango/')
+insert into LinkFork(IdProjeto, IdMembro, URL) values (1, 4, '/bernardobrezende/exemplos-pti/')
+insert into LinkFork(IdProjeto, IdMembro, URL) values (1, 4, '/bernardobrezende/gulp-useref/')
