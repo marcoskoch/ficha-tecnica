948bb8e60a23ba727e07aabb485c5cdff649bab2
Felipe Ribeiro De Moraes <felipemoraes920@gmail.com>
2015-12-05 03:41:09 -0300

    Adicionado MembroDetalheProjetoModel

---
+++ FichaTecnica/FichaTecnica/Models/MembroDetalheProjetoModel.cs
@@ -0,0 +1,27 @@
+﻿using FichaTecnica.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FichaTecnica.Models
+{
+    public class MembroDetalheProjetoModel
+    {
+        public string Foto { get; set; }
+        public Cargo cargo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public ICollection<Projeto> Projetos { get; set; }
+        public List<LinkFork> LinksGithub { get; set; }
+
+        public MembroDetalheProjetoModel(Membro entity)
+        {
+            this.Foto = entity.Foto;
+            this.cargo = entity.Cargo;
+            this.Nome = entity.Nome;
+            this.Email = entity.Email;
+            this.Projetos = entity.Projetos;
+        }
+    }
+}
