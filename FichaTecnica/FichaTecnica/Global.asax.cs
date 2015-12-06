312999a199832af8c89aa08d29c694ee411354f4
Marcos Koch <marcos_koch@hotmail.com>
2015-12-05 23:35:32 -0200

    feat(Otimização) adicionado Bundle ao projeto;

--- FichaTecnica/FichaTecnica/Global.asax.cs
+++ FichaTecnica/FichaTecnica/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using FichaTecnica.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace FichaTecnica
@@ -13,6 +15,9 @@ namespace FichaTecnica
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
