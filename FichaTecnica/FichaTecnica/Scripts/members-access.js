276ee8f78ef18e27914f0f2375f90ff159dc41a8
Renan Vaz <renanp.vaz@outlook.com>
2015-12-08 21:06:39 -0200

    separar script da tela de detalhes do projeto

--- FichaTecnica/FichaTecnica/Scripts/members-access.js
+++ FichaTecnica/FichaTecnica/Scripts/members-access.js
@@ -2,33 +2,34 @@
     var model = (membro.LinksGithub);
     var commitList = [];
 
-    model.Links.forEach(function (l) {
+    model.Links.forEach(function(l) {
         var localStorageCommits = localStorage.getItem('latestCommits'.concat(membro.Id.toString()));
 
-        if (localStorageCommits == null || localStorageCommits == '[]') {
+        if(localStorageCommits == null || localStorageCommits == '[]') {
             $.get(model.URLBaseAPI + l.URL + 'commits')
                 .done(function (response) {
                     response.forEach(function (i) {
                         commitList.push({ sha: i.sha.slice(0, 7), date: new Date(i.commit.author.date).getTime(), message: i.commit.message, link: i.html_url })
+
                     })
                 })
-            .always(function () {
-                atribuiValoresGitHubAoMembro(commitList, membro);
+            .always(function() {
+                atribuiValoresGitHubAoMembro(commitList,membro);
             });
         }
-        else {
+        else{
             commitList = JSON.parse(localStorageCommits);
-            atribuiValoresGitHubAoMembro(commitList, membro);
+            atribuiValoresGitHubAoMembro(commitList,membro);
         }
     });
 });
 
-function atribuiValoresGitHubAoMembro(commitList, membro) {
+function atribuiValoresGitHubAoMembro(commitList, membro){
     commitList.sort(function (left, right) { return left.date < right.date })
     commitList = commitList.slice(0, 3);
     localStorage.setItem('latestCommits'.concat(membro.Id), JSON.stringify(commitList));
-    commitList.forEach(function (commit) {
-        var timeStamp = new Date(commit.date);
+    commitList.forEach(function(commit) {
+        var timeStamp  = new Date(commit.date);
         membro.DataUltimoCommit = Date.parse(timeStamp);
 
         membro.CommitSHA = commit.sha;
@@ -37,155 +38,194 @@ function atribuiValoresGitHubAoMembro(commitList, membro) {
     })
 };
 
-setTimeout(function () {
+setTimeout(function(){
     membros.sort(compare);
-}, 1000);
+},1000);
 
 
-function compare(a, b) {
-    if (a.DataUltimoCommit > b.DataUltimoCommit) {
+function compare(a,b) {
+    if (a.DataUltimoCommit > b.DataUltimoCommit){
         return -1;
     }
-    if (a.DataUltimoCommit < b.DataUltimoCommit) {
+    if (a.DataUltimoCommit < b.DataUltimoCommit){
         return 1;
     }
     return 0;
 };
 
-function removeEspacos(membro) {
+function removeEspacos(membro){
 
-    membro.Nome = membro.Nome.replace(/\s/g, "XXXXXX");
+    membro.Nome = membro.Nome.replace(/\s/g, "XZYXZY") ;
 }
 
-function atribuiEspacos(membro) {
-    membro.Nome = membro.Nome.replace(/XXXXXX/g, ' ');
+function atribuiEspacos(membro){
+    membro.Nome = membro.Nome.replace(/XZYXZY/g, ' ');
 }
 
-setTimeout(function () {
+setTimeout(function() {
     membros.forEach(function (membro) {
         removeEspacos(membro);
-        $('<div><div/>').attr('id', ("Cartao" + membro.Nome))
-        .addClass('Cartao hvr-push hvr-pulse:hover, .hvr-pulse:focus, .hvr-pulse:active')
+        $('<div><div/>').attr('id', ("Cartao"+ membro.Nome))
+        .addClass('Cartao')
         .appendTo('#membrosProjeto');
         var caminhodivCartao = "Cartao" + (membro.Nome);
 
-        $('<div></div>').attr('id', "content" + membro.Nome)
+        $('<div></div>').attr('id',"content"+membro.Nome)
         .addClass('content')
-        .appendTo('#' + caminhodivCartao);
+        .appendTo('#'+caminhodivCartao);
 
         var caminhodivContent = "content" + (membro.Nome);
 
-        $('<div></div>').attr('id', "imagem" + membro.Nome)
+        $('<div></div>').attr('id', "imagem"+membro.Nome)
         .addClass('imagem')
-        .appendTo('#' + caminhodivContent);
+        .appendTo('#'+caminhodivContent);
 
         var caminhodivImagem = "imagem" + (membro.Nome);
 
-        var fotoMembro = $('<img id="loading" src="/Upload/img/' + membro.Foto + '" alt="..." class="img-circle" />');
+        var fotoMembro  = $('<img id="loading" src="/Upload/img/'+membro.Foto+'" alt="..." class="img-circle " />');
 
-        $('#' + caminhodivImagem).append(fotoMembro);
+        $('#'+caminhodivImagem).append(fotoMembro);
 
-        $('<div></div>').attr('id', "Cargo" + membro.Nome)
+        $('<div></div>').attr('id',"Cargo"+membro.Nome)
         .addClass('resumoMembro')
-        .appendTo('#' + caminhodivContent);
+        .appendTo('#'+caminhodivContent);
 
-        var caminhodivCargo = "Cargo" + (membro.Nome);
+        var caminhodivCargo = "Cargo" +  (membro.Nome);
 
         atribuiEspacos(membro);
         $('<p></p>')
-        .html('Nome: ' + membro.Nome)
-        .appendTo('#' + caminhodivCargo);
+        .attr('id','divNomeMembro' + membro.Nome)
+        .html(membro.Nome)
+        .addClass('resumoMembro-nomeMembro')
+        .appendTo('#'+caminhodivCargo);
         removeEspacos(membro);
 
         $('<p></p>')
-       .html('Email: ' + membro.Email)
-       .appendTo('#' + caminhodivCargo);
+       .html('Email: '+ membro.Email)
+       .appendTo('#'+caminhodivCargo);
 
         $('<p></p>')
-       .html('Cargo: ' + membro.cargo.Descricao)
-       .appendTo('#' + caminhodivCargo);
+       .html('Cargo: '+membro.cargo.Descricao)
+       .appendTo('#'+caminhodivCargo);
 
-        $('<div></div>').attr('id', "campos" + membro.Nome)
+        var caminhoDivNomeMmebro = "divNomeMembro" + membro.Nome;
+
+        $('<div></div>').attr('id', "comentariosPositivos"+membro.Nome)
+        .attr('style', 'float:left')
+        .attr('style', 'background-repeat:no-repeat')
+        .addClass('campos-Positivo positivo')
+        .appendTo('#'+caminhodivContent);
+
+        var caminhoComentariosPositivos =  "comentariosPositivos" + (membro.Nome);
+
+        $('<div></div>').html(membro.TotalComentariosPosivo)
+
+       .appendTo('#'+caminhoComentariosPositivos);
+
+        $('<div></div>').attr('id', "comentariosNegativos"+membro.Nome)
+        .attr('style', 'float:right')
+        .attr('style', 'background-repeat:no-repeat')
+        .addClass('campos-Negativo negativo')
+        .appendTo('#'+caminhodivContent);
+
+
+        var caminhoComentariosNegativos = "comentariosNegativos" + (membro.Nome);
+
+        $('<div></div>')
+        .attr('id', "TotalNegativos"+membro.Nome)
+        .html(membro.TotalComentarioNegativo)
+        .appendTo('#'+caminhoComentariosNegativos);
+
+        var caminhoTotalNegativos =  "TotalNegativos" + (membro.Nome);
+
+        $('<div></div>').html('')
+        .attr('id', 'LinhaLateral'+membro.Nome)
+        .attr('style', 'float:right')
+        .addClass('linhaVertical')
+        .appendTo('#'+caminhodivContent);
+
+        var caminhoParaLinhaLateral = "LinhaLateral" + membro.Nome;
+
+        $('<div></div>')
+        .attr('id', 'divLateral' + membro.Nome)
+        .addClass('divLateral')
+        .appendTo('#'+caminhodivContent);
+
+        var caminhoParaDivLateral = "divLateral" + membro.Nome;
+
+        $('<div></div>')
+        .addClass('mega-octicon octicon-mark-github corIconGitHub')
+        .appendTo('#'+caminhoParaDivLateral)
+
+        $('<div></div>').attr('id', "campos"+membro.Nome)
         .addClass('campos')
-        .appendTo('#' + caminhodivContent);
+        .appendTo('#'+caminhodivContent);
 
         var caminhodivCampos = "campos" + (membro.Nome);
 
         $('<div></div>').attr('id', (membro.Nome))
-        .html('Último commit:')
-        .appendTo('#' + caminhodivCampos);
-
-        $('<div></div>').attr('id', "dataUltimoCommit" + membro.Nome)
-        .html('Data Último commit:')
-        .appendTo('#' + caminhodivCampos);
-
-        var caminhoDataUltimocommit = "dataUltimoCommi" + (membro.Nome);
-
-        $('<div></div>').attr('id', "DataUltimoCommit" + membro.Nome)
-        .appendTo('#' + caminhodivCampos);
-
-        $('<p></p>').html('Comentarios')
-        .appendTo('#' + caminhodivCampos);
-
-        $('<div></div>').attr('id', "comentariosPositivos" + membro.Nome)
-        .attr('style', 'float:left')
-        .html('Positivos / ')
-        .appendTo('#' + caminhodivCampos);
-
-        var caminhoComentariosPositivos = "comentariosPositivos" + (membro.Nome);
-
-        $('<div></div>').html(membro.TotalComentariosPosivo)
-        .appendTo('#' + caminhoComentariosPositivos);
-
-        $('<div></div>').attr('id', "comentariosNegativos" + membro.Nome)
-        .html(' Negativos:')
-        .appendTo('#' + caminhodivCampos);
-
-        var caminhoComentariosNegativos = "comentariosNegativos" + (membro.Nome);
-
-        $('<div></div>').html(membro.TotalComentarioNegativo)
-        .appendTo('#' + caminhoComentariosNegativos);
-
-        var form = $("<form/>", { action: '/Membro/FichaMembro' });
-        form.append($("<input>", { type: 'hidden', name: 'id', value: membro.Id }));
-        form.append($("<input>", { type: 'submit', value: 'Ficha Completa', name: 'enviar' }));
-        $(form).appendTo('#' + caminhodivCampos);
+        .html('Último commit no GitHub:')
+        .addClass('divMenssagemUltimoCommit')
+        .appendTo('#'+caminhoParaDivLateral);
 
         var caminhoComiits = (membro.Nome);
-        $('#' + caminhoComiits)
-            .append($('<div>')
-                .append($('<p>')
-                    .append($('<a>').html(membro.CommitSHA).attr('href', membro.CommitLink))
-                    .append($('<p>').html(membro.Commitmenssage)))
+        $('#'+caminhoComiits)
+        .attr('id', 'LinkUltimoCommit'+ membro.Nome)
+        .append($('<div>')
+        .append($('<p>')
+        .append($('<a>').attr('style', 'color: #ff3333').html(membro.CommitSHA).attr('href', membro.CommitLink))
+        .append($('<p>').html(membro.Commitmenssage))))
 
-        )
-        var oneDay = 24 * 60 * 60 * 1000;
+        var caminhoLinkUltimoCommit = "LinkUltimoCommit" + membro.Nome;
+
+        $('<div></div>').attr('id', "dataUltimoCommit"+membro.Nome)
+        .html('Data Último commit:')
+        .appendTo('#'+caminhoLinkUltimoCommit);
+
+        var caminhoDataUltimocommit = "dataUltimoCommit" +(membro.Nome);
+
+        $('<div></div>').attr('id', "DataUltimoCommit"+membro.Nome)
+        .appendTo('#'+caminhoDataUltimocommit);
+
+        var oneDay = 24*60*60*1000;
         var diaAtual = new Date();
         var diaUltimoCommit = new Date(membro.DataUltimoCommit);
-        var diasDiferenca = Math.round(Math.abs((diaAtual.getTime() - diaUltimoCommit.getTime()) / (oneDay)));
+        var diasDiferenca = Math.round(Math.abs((diaAtual.getTime() - diaUltimoCommit.getTime())/(oneDay)));
 
-        if (diasDiferenca > 1) {
-            var caminhoParaFichaMembro = "Cartao" + (membro.Nome);
-            $('#' + caminhoParaFichaMembro)
-            .addClass('mudarCorFicha');
+        if(diasDiferenca > 1){
+            var warningIcon  = $('<img id="loadingIcon" src="/Content/Image/caution8.png" alt="..." class="warningIcon" title="Membro há mais de 1 dia sem commitar" />');
+
+            $('#'+caminhoParaDivLateral)
+            .append(warningIcon);
         }
 
-        var dia = diaUltimoCommit.getUTCDay();
-        var mes = diaUltimoCommit.getUTCMonth() + 1;
-        var ano = diaUltimoCommit.getUTCFullYear();
-        var hora = diaUltimoCommit.getUTCHours();
-        var minutos = diaUltimoCommit.getUTCMinutes();
-
-        var dataEHoraUltimoCommit = dia + '/' + mes + '/' + ano + ' - ' + hora + ':' + minutos;
+        dataEHoraUltimoCommit = montaData(diaUltimoCommit);   
 
         var caminhoDataUltimoCommit = "DataUltimoCommit" + (membro.Nome);
-        $('#' + caminhoDataUltimoCommit)
-        .append('<p>').html(dataEHoraUltimoCommit);
+        $('<p></p>')
+            .attr('style', 'float:left')
+        .html(dataEHoraUltimoCommit)
+        .appendTo('#'+caminhoDataUltimoCommit);
+        
+
+        $(document).ready(function(){
+            $('#Cargo'+ membro.Nome).click(function (){
+                window.location = "/Membro/FichaMembro/" + membro.Id;
+            });
+        });
+
+        $(document).ready(function(){
+            $('#divLateral'+ membro.Nome).click(function (){
+                window.location = membro.CommitLink
+            });
+        });
+
+        $('#Carregando').hide();
     })
 
 }, 2000);
 
 
-setInterval(function () {
+setInterval(function() {
     localStorage.clear();
 }, 1000 * 60 * 10)
